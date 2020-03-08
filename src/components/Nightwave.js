24448a679ef9792133044a0fb8dc368bbdd65798
DrewTAJ <drew.t.a.johnson@gmail.com>
2020-03-08 19:36:32 -0400

    Added cards for nightwave challenges

--- src/components/Nightwave.js
+++ src/components/Nightwave.js
@@ -1,35 +1,54 @@
 import React from 'react';
 import { Row, Col, Card } from 'react-bootstrap';
+import Countdown from 'react-countdown';
 
 export class Nightwave extends React.Component {
 
 
-    getChallenges() {
-        if(!this.props.nightwaveSeason.activeChallenges) return;
-        return this.props.nightwaveSeason.activeChallenges.map((value, index) => {
-            return;
+    getChallenges(challenges) {
+        if(challenges.length === 0) return;
+        return challenges.map((value, index) => {
+            const startDate = new Date(value.expiry);
+            return (
+                <Col xs="3" key={value.id}>
+                    <Card>
+                        <Card.Body>
+                            <p>{value.desc}</p>
+                            <p>{value.reputation}</p>
+                            <Countdown key={value.id} daysInHours={true} date={startDate} />
+                        </Card.Body>
+                    </Card>
+                </Col>
+            );
         });
     }
 
     render() {
-
         const dailies = [];
         const challenges = [];
-
-        for(let i = 0; i < this.props.nightwaveSeason.activeChallenges; i++) {
-            if(this.props.nightwaveSeason.activeChallenges[i].isDaily) {
-                this.dailies.push(this.props.nightwaveSeason.activeChallenges[i]);
-            } else {
-                this.challenges.push(this.props.nightwaveSeason.activeChallenges[i]);
+        if(this.props.nightwaveSeason.activeChallenges) {
+            for(let i = 0; i < this.props.nightwaveSeason.activeChallenges.length; i++) {
+                if(this.props.nightwaveSeason.activeChallenges[i].isDaily) {
+                    dailies.push(this.props.nightwaveSeason.activeChallenges[i]);
+                } else {
+                    challenges.push(this.props.nightwaveSeason.activeChallenges[i]);
+                }
             }
         }
 
         return(
             <Col size="12" key={this.props.nightwaveSeason.id}>
+                <h2>NightWave</h2>
                 <Card>
                     <Card.Body>
-                        <Card.Title>Nightwave</Card.Title>
-                        {this.getChallenges()}
+                        <Card.Title>Dailies</Card.Title>
+                        <Row>{this.getChallenges(dailies)}</Row>
+                    </Card.Body>
+                </Card>
+                <Card>
+                    <Card.Body>
+                        <Card.Title>Challenges</Card.Title>
+                        <Row>{this.getChallenges(challenges)}</Row>
                     </Card.Body>
                 </Card>
             </Col>
